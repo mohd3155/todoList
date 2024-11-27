32734d03b14e62e7d7f43261b24946847a29cb20
Mohammed <moh9_kamal@hotmail.com>
2024-11-28 00:51:53 +0200

    Add Playwright end-to-end test for adding, completing, and verifying tasks in the todo list - Implemented a test that simulates adding a new todo item. - Changed its status to 'Completed' and verified it appears in the 'Completed' tasks list. - Ensured proper interaction with UI elements such as task input, status dropdown, and task filter.

--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -4,20 +4,19 @@ import TasksContext from "../contexts/tasksContexts"
 const headers = ["Task", "Description", "Status", "Change status", ""]
 
 const TasksList = () => {
-    const{tasks , dispatch} = useContext(TasksContext)    
+    const{tasks , dispatch} = useContext(TasksContext)  
     
     const changeStatus = (value:string , taskId:number) =>  dispatch({type:"CHANGE", value:value, taskId: taskId})
     const handleDelete = (taskId : number) => dispatch({type:"DELETE" , taskId:taskId})
     const handleFilter = (value:string) => dispatch({type:"FILTER", value:value})
     
-    console.log(tasks)
 
     return (
         <div className="2xl:w-3/4 sm:w-4/5 xs:w-11/12 2xl:h-3/5 sm:h-3/5 xs:h-3/4 sm:flex 2xl:flex-row xs:flex-col 2xl:justify-center 2xl:items-center xs:mt-5">
             <div className="2xl:w-full 2xl:h-full 2xl:flex 2xl:flex-row xs:flex-col 2xl:justify-between" >
             <div className="2xl:w-1/12 sm:w-2/4 xs:w-full 2xl:h-1/6 2xl:border-b-2 2xl:border-r-2 xs:border-r-0 2xl:border-white 2xl:pr-2 ">
                 <label className="2xl:w-full xs:text-white xs:font-bold">Filter By :</label>
-                <select onChange={(e) => handleFilter(e.target.value)} className="2xl:w-full xs:w-2/4 xs:bg-[#1e2732] xs:text-white">
+                <select data-testid="filter" onChange={(e) => handleFilter(e.target.value)} className="2xl:w-full xs:w-2/4 xs:bg-[#1e2732] xs:text-white">
                     <option value="all">All</option>
                     <option value="Active">Active</option>
                     <option value="Completed">Completed</option>
@@ -30,12 +29,12 @@ const TasksList = () => {
                 <div key={index+1} className="2xl:w-1/4 xs:w-1/5 2xl:h-full flex xs:justify-center xs:border-b-2  xs:border-white xs:items-center"><span className="w-full xs:w-full text-2xl font-bold xs:text-xs sm:text-base lg:text-2xl xs:truncate xs:text-center text-white">{header}</span></div>
             )}
             </div>
-            {tasks.map((task) => <div key={task.id} className="2xl:w-full xs:w-full 2xl:h-1/6 xs:h-full xs:flex xs:flex-row xs:justify-between xs:items-center xs:border-b-2 xs:border-white xs:mt-3 xs:pb-3">
-                <div className="2xl:w-1/4 xs:w-1/5 2xl:h-full xs:flex xs:justify-center xs:items-center  "><span className="2xl:w-full xs:max-w-full xs:text-center lg:text-lg xs:text-white xs:text-xs xs:truncate 2xl:font-bold">{task.task}</span></div>
-                <div className="2xl:w-1/4 xs:w-1/5 2xl:h-full xs:flex xs:justify-center xs:items-center break-words"><span className="2xl:w-full xs:max-w-full xs:text-xs lg:text-lg xs:text-center  line-clamp-3  xs:text-white">{task.description}</span></div>
-                <div className="2xl:w-1/4 xs:w-1/5 2xl:h-full xs:flex xs:justify-center xs:items-center "><span className={`2xl:w-full xs:w-full xs:text-center xs:text-xs lg:text-lg lg:font-bold xs:truncate ${task.status === "Active" ? "text-red-600": "text-green-600"}`}>{task.status}</span></div>
+            {tasks.map((task) => <div data-testid="tasksList" key={task.id} className="2xl:w-full xs:w-full 2xl:h-1/6 xs:h-full xs:flex xs:flex-row xs:justify-between xs:items-center xs:border-b-2 xs:border-white xs:mt-3 xs:pb-3">
+                <div data-testid={task.status === "Active" ? "Active-tasksList-tasks" : "Completed-tasksList-tasks"} className="2xl:w-1/4 xs:w-1/5 2xl:h-full xs:flex xs:justify-center xs:items-center  "><span className="2xl:w-full xs:max-w-full xs:text-center lg:text-lg xs:text-white xs:text-xs xs:truncate 2xl:font-bold">{task.task}</span></div>
+                <div data-testid={task.status === "Active" ? "Active-tasksList-desc" : "Completed-tasksList-desc"} className="2xl:w-1/4 xs:w-1/5 2xl:h-full xs:flex xs:justify-center xs:items-center break-words"><span className="2xl:w-full xs:max-w-full xs:text-xs lg:text-lg xs:text-center  line-clamp-3  xs:text-white">{task.description}</span></div>
+                <div data-testid={task.status === "Active" ? "Active-tasksList-status" : "Completed-tasksList-status"} className="2xl:w-1/4 xs:w-1/5 2xl:h-full xs:flex xs:justify-center xs:items-center "><span className={`2xl:w-full xs:w-full xs:text-center xs:text-xs lg:text-lg lg:font-bold xs:truncate ${task.status === "Active" ? "text-red-600": "text-green-600"}`}>{task.status}</span></div>
                 <div className="2xl:w-1/4 xs:w-1/5 2xl:h-full xs:flex xs:justify-center xs:items-center ">
-                    <select className="lg:w-2/4 xs:w-full xs:bg-[#1e2732]  xs:text-sm lg:text-lg xs:text-white"  onChange={(e) => changeStatus(e.target.value,task.id)}>
+                    <select data-testid = "change-status" className="lg:w-2/4 xs:w-full xs:bg-[#1e2732]  xs:text-sm lg:text-lg xs:text-white"  onChange={(e) => changeStatus(e.target.value,task.id)}>
                         <option value="Active" className="xs:text-sm">Active</option>
                         <option value="Completed">Completed</option>
                     </select>
---
+++ tests/addTasks.spec.ts
@@ -0,0 +1,46 @@
+import {  test, expect, type Page } from '@playwright/test';
+
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173/');
+});
+
+test("has title", async ({page}) =>{
+    await expect(page).toHaveTitle(/List/);
+})
+
+const tasks = [{
+    id:1,
+    task:"math",
+    description:"math task 1",
+    status : "Active"
+}] as const
+
+    test("change the status to completed and verify it appears in the completed tasks list",async ({page})=>{
+        await page.getByPlaceholder("Task Title").fill(tasks[0].task);
+        await page.getByPlaceholder("Description").fill(tasks[0].description);
+        await page.getByRole("status").selectOption(tasks[0].status);
+        await page.getByRole("button").click();
+        
+        await page.getByTestId("change-status").selectOption("Completed")
+        await expect(page.getByTestId("Completed-tasksList-status")).toHaveText([
+            "Completed",
+        ]);
+        
+        await page.getByTestId('filter').selectOption("Completed");
+
+        await page.waitForSelector('[data-testid="Completed-tasksList-tasks"]');
+        await page.waitForSelector('[data-testid="Completed-tasksList-desc"]');
+        await page.waitForSelector('[data-testid="Completed-tasksList-status"]');
+
+        await expect(page.getByTestId("Completed-tasksList-tasks")).toHaveText([
+            tasks[0].task
+        ])
+        await expect(page.getByTestId("Completed-tasksList-desc")).toHaveText([
+            tasks[0].description
+        ])
+        await expect(page.getByTestId("Completed-tasksList-status")).toHaveText([
+            "Completed"
+        ])
+    })
+  
